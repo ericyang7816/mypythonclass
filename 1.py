322d5e3321b373c85eb6668f5a0c8c69b00545fa
ericyang7816 <ericyang7816@foxmail.com>
2019-09-15 23:07:28 +0800

    Create 1.py
    My first work

---
+++ 1.py
@@ -0,0 +1,23 @@
+str1=input("Please input a word:")
+print(str1)
+str2=input("Please input a number:")
+print(int(str2))
+print(" ")
+
+number1=5
+print("5是")
+if number1>0:
+    print("正数")
+elif number1<0:
+    print("负数")
+else:
+    print("0")
+print(" ")
+    
+number2=int(input("Please enter a number:"))
+if number2>0:
+    print("正数")
+elif number2<0:
+    print("负数")
+else:
+    print("0")
