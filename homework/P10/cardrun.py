52a5b7f6e0aa0b4768bfc16ae836b8c2c028ad37
ericyang7816 <ericyang7816@foxmail.com>
2019-11-06 14:50:17 +0800

    P10-Q1 finished

---
+++ homework/P10/cardrun.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+
+def safe_float(object):
+    'safe version of float()'
+    try:
+        retval = float(object)
+    except (TypeError, ValueError) as diag:
+        retval = str(diag)
+    return retval
+
+
+def main():
+    'handles all the data processing'
+    log = open('cardlog.txt', 'w')
+    try:
+        ccfile = open('carddata.txt', 'r')
+    except IOError:
+        log.write('No transactions this month \n')
+        log.close()
+        return
+    transactions = ccfile.readlines()
+    ccfile.close()
+    total = 0.00
+    log.write('account log:\n')
+
+    for each_txns in transactions:
+        result = safe_float(each_txns)
+        if isinstance(result, float):
+            total += result
+            log.write('data...processed\n')
+        else:
+            log.write('ignored: %s' % result)
+    print('$%.2f (new balance)'%total)
+    log.close()
+
+if __name__ == '__main__':
+    main()
